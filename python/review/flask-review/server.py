11e28e05e6431d6cac8ab23ee8a9193990b5359c
Oderystella <ddott59@gmail.com>
2021-03-08 10:45:53 +0900

    feat: addtional route for users

--- python/review/flask-review/server.py
+++ python/review/flask-review/server.py
@@ -3,9 +3,16 @@ from flask import flask
 
 app = Flask(__name__)
 
+# serve.string
 @app.route('/')
 def index():
     return 'Flask works'
+
+#addtional route with json
+@app.route('/users')
+def show_users():
+    return {'users:['John Doe', 'Jane Doe']}
+
 
 if __name__=='__main__':
     app.run(host='localhost', port=8080, debug=True)
