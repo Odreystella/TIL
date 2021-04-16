e2233f01a1ac361417f0222bf2d04a847499fce0
Oderystella <ddott59@gmail.com>
2021-04-16 17:32:15 +0900

    feat: add function of getting movie code

--- django/review/web_scraping/naver_movie.py
+++ django/review/web_scraping/naver_movie.py
@@ -27,7 +27,51 @@ def get_movie_list():
 
     return movies_list
 
-print(get_movie_list())
+movies_list = get_movie_list()
+# print(movies_list)
+
+# 영화 제목 입력하면 code를 출력하는 함수
+def get_movie_code(movie_title):
+    for movie in movies_list:
+        if movie_title == movie['movie_title']:
+            return movie['code']
+
+print(get_movie_code('서복'))
+movie_code = get_movie_code('서복')
+
+# 영화 코드를 입력하면 리뷰를 출력하는 함수
+def get_review_of_movie():
+
+    headers = {
+        'authority': 'movie.naver.com',
+        'sec-ch-ua': '" Not A;Brand";v="99", "Chromium";v="90", "Google Chrome";v="90"',
+        'sec-ch-ua-mobile': '?0',
+        'upgrade-insecure-requests': '1',
+        'user-agent': 'Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/90.0.4430.72 Safari/537.36',
+        'accept': 'text/html,application/xhtml+xml,application/xml;q=0.9,image/avif,image/webp,image/apng,*/*;q=0.8,application/signed-exchange;v=b3;q=0.9',
+        'sec-fetch-site': 'same-origin',
+        'sec-fetch-mode': 'navigate',
+        'sec-fetch-dest': 'iframe',
+        'referer': 'https://movie.naver.com/movie/bi/mi/point.nhn?code=159074',
+        'accept-language': 'en-US,en;q=0.9,ko;q=0.8',
+        'cookie': 'NRTK=ag#all_gr#1_ma#-2_si#0_en#0_sp#0; NNB=WPUBCSBSZUHGA; ASID=6af24fe6000001774c0e192700005e55; _fbp=fb.1.1612100117041.1815354310; _ga=GA1.2.867914976.1612100117; nx_ssl=2; notSupportBrowserAlert=true; BMR=s=1618539655670&r=https%3A%2F%2Fm.blog.naver.com%2Fsamojun%2F221766166563&r2=https%3A%2F%2Fwww.google.com%2F; NV_WETR_LOCATION_RGN_M="MDkxNDAxMDQ="; NV_WETR_LAST_ACCESS_RGN_M="MDkxNDAxMDQ="; page_uid=hcCibwp0YidssPPKn2NssssstMo-105488; csrf_token=cbf59227-d901-4d10-b024-89ef04ca33fc',
+    }
+
+    params = (
+        ('code', '159074'),
+        ('type', 'after'),
+        ('isActualPointWriteExecute', 'false'),
+        ('isMileageSubscriptionAlready', 'false'),
+        ('isMileageSubscriptionReject', 'false'),
+    )
+
+    response = requests.get('https://movie.naver.com/movie/bi/mi/pointWriteFormList.nhn', headers=headers, params=params)
+
+
+    
+
+
+    
 
 
     
