ba031bccf5fded3811f823d21a1c95a772ec918e
Oderystella <ddott59@gmail.com>
2021-04-26 16:10:07 +0900

    feat: add function of verifying name

--- django/review/finding_name/adminapp/views.py
+++ django/review/finding_name/adminapp/views.py
@@ -1,3 +1,20 @@
 from django.shortcuts import render
 
+students_db = ['오지윤', '이한결', '박찬경', '박찬종', '김주형']
 # Create your views here.
+def index(request):
+    return render(request, 'index.html')
+
+def verify(request):
+    if request.method =='POST':
+        username = request.POST['username']
+        input_time = request.POST['time']
+        context = {
+                'username' : username,
+                'time' : input_time,
+                'verify' : False,
+        }
+        if username in students_db:
+           context['verify'] = True
+           
+    return render(request, 'verify.html', context)
