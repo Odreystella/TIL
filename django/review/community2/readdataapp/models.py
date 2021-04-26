2f90ceb1a9f409e4257dbb136491c4aeb2adb52e
Oderystella <ddott59@gmail.com>
2021-04-27 01:23:47 +0900

    feat: show category list at index page

--- django/review/community2/readdataapp/models.py
+++ django/review/community2/readdataapp/models.py
@@ -1,3 +1,18 @@
 from django.db import models
 
 # Create your models here.
+class Category(models.Model):
+    name = models.CharField(max_length=64)
+
+    def __str__(self):
+        return self.name
+
+class Article(models.Model):
+    category = models.ForeignKey(Category, on_delete=models.CASCADE, related_name='article')
+    writer = models.CharField(max_length=64)
+    title = models.CharField(max_length=64)
+    contents = models.TextField()
+    date = models.DateTimeField()
+
+    def __str__(self):
+        return self.title
--- django/review/community2/readdataapp/views.py
+++ django/review/community2/readdataapp/views.py
@@ -1,3 +1,11 @@
 from django.shortcuts import render
+from .models import Category, Article
 
 # Create your views here.
+def index(request):
+    category_list = Category.objects.all()
+    # print(category_list)
+    context = {
+        'category_list' : category_list,
+    }
+    return render(request, 'index.html', context)
